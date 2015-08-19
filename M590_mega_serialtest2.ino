b1b2deae8ccd94f132d2ba4856d1a320651ca0ae
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2015-08-19 13:06:19 +0300

    Update M590_mega_serialtest2.ino

--- M590_mega_serialtest2.ino
+++ M590_mega_serialtest2.ino
@@ -2,7 +2,7 @@
 // rewrite after phote by niq_ro from http://nicuflorica.blogspot.com/
 // and http://arduinotehniq.blogspot.com/
 // and http://www.tehnic.go.ro
-// rewrite for M590 GPRS module by niq_ro - 13.08.2015, Craiova - Romania, Europe, Earth
+// rewrite for M590 GPRS module by niq_ro - 18.08.2015, Craiova - Romania, Europe, Earth
 
 void setup() {
   // initialize both serial ports:
